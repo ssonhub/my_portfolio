bbebe062e407cfb5d82a87ad3256245260f0e7ed
ssonhub <58754723+ssonhub@users.noreply.github.com>
2022-09-30 01:09:38 -0700

    row alignment

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -7,8 +7,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+<<<<<<< Updated upstream
       body: Container(
         child: Text("my text"),
+=======
+      body: SafeArea(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              IntroCard(text: "hi"),
+              IntroCard(text: "hi"),
+              IntroCard(text: "hi"),
+            ],
+          ),
+        ),
+>>>>>>> Stashed changes
       ),
     );
   }
